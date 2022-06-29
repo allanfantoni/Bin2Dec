6ef5cb53e75b8273fef785b4950b76d1474af7b8
Allan Fantoni <allan.fantoni@outlook.com>
2022-06-29 12:58:58 -0300

    New messages, possibility to convert n binary numbers in sequence.

--- Bin2Dec/Bin2Dec/Program.cs
+++ Bin2Dec/Bin2Dec/Program.cs
@@ -8,18 +8,36 @@ namespace Bin2Dec
         {
             try
             {
-                Console.Write("Enter up to 8 binary digits: ");
-                var binaryDigits = Console.ReadLine();
+                Console.Write("How many binary numbers do you want to convert? ");
+                var quantity = int.Parse(Console.ReadLine());
 
-                if (binaryDigits.Length > 8)
-                    throw new OverflowException("binary number over 8 digits.");
+                List<string> list = new();
 
-                var containOnlyBinary = Regex.IsMatch(binaryDigits.Trim(), "[^01]");
+                Console.WriteLine("");
 
-                if (containOnlyBinary)
-                    throw new IOException("binary number with different characters.");
+                for (int i = 1; i <= quantity; i++)
+                {
+                    Console.WriteLine($"#{i}");
+                    Console.Write("Enter up to 8 binary digits: ");
+                    var binaryDigits = Console.ReadLine();
 
-                Console.WriteLine(Convert.ToInt32(binaryDigits, 2));
+                    if (binaryDigits.Length > 8)
+                        throw new OverflowException("binary number over 8 digits.");
+
+                    var containsOtherCharacters = Regex.IsMatch(binaryDigits.Trim(), "[^01]");
+
+                    if (containsOtherCharacters)
+                        throw new IOException("binary number with different characters.");
+
+                    list.Add(binaryDigits.Trim());
+
+                    Console.WriteLine("---");
+                }
+
+                Console.WriteLine("Binaries converted to Decimals:");
+
+                foreach (var item in list)
+                    Console.WriteLine(Convert.ToInt32(item, 2));
             }
             catch (Exception ex)
             {
